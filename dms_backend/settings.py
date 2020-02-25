54b0034db5713a6ef808d319d8809ad97030352f
Benjamin Schmidt <benschmidt@live.de>
2020-02-25 14:15:01 +0100

    add data model

--- dms_backend/settings.py
+++ dms_backend/settings.py
@@ -47,6 +47,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'data.apps.DataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
