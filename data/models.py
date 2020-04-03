17a5d6b26159671376b37cd1aede8b458394c5b9
Benjamin Schmidt <benschmidt@live.de>
2020-04-03 19:22:04 +0200

    cleaner version, removed unnecessary stuff

--- data/models.py
+++ data/models.py
@@ -6,38 +6,19 @@ import string
 from pathlib import Path
 
 from django.db import models
-from django.utils import timezone
+from django.utils import timezone, dateformat
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-tab01 = pd.read_csv('http://www.uc2-program.org/uc2_table_A1.csv', sep="\t").reset_index()
-tab02 = pd.read_csv('http://www.uc2-program.org/uc2_table_A2.csv', sep="\t").reset_index()
-tab03 = pd.read_csv('http://www.uc2-program.org/uc2_table_A3.csv', sep="\t").reset_index()
-tab04 = pd.read_csv('http://www.uc2-program.org/uc2_table_A4.csv', sep="\t").reset_index()
-
-VARIABLE = [vari for vari in zip(tab01.iloc[:, 0], tab01.loc[:, 'variable'])]
-DATA_CONTENT = [daco for daco in zip(tab02.iloc[:, 0], tab02.loc[:, 'data_content'])]
-INSTITUTION = [inst for inst in zip(tab03.iloc[:, 0], tab03.loc[:, 'institution'])]
-SITE = [site for site in zip(tab04.iloc[:, 0], tab04.loc[:, 'site'])]
-LICENCE = [("test", "Choose a License"), ("UC2", "[UC]2 Open Licence")]
-DATA_TYPE = [
-    ("", "What data type do you want to upload?"),
-    ("UC2Obs", "UC2Observation"),
-    ("UC2Mod", "UC2Model"),
-    ("sta_dr", "Static Driver"),
-    ("dyn_in", "Dynamic Driver"),
-]
-
 
 def get_sentinel_user():
-    return get_user_model().objects.get_or_create(username='deleted')[0]
+    return get_user_model().objects.get_or_create(username="deleted")[0]
 
 
 class DataFile(models.Model):
-    data_type = models.CharField(max_length=200, choices=DATA_TYPE, default="trajectory")
-    input_name = models.CharField(max_length=200, default="test")
-    file_id = models.CharField(max_length=200, default="0")
-    file_path = models.FileField(max_length=200, null=False, unique=True, upload_to='files/', default=settings.BASE_DIR)
+    data_type = models.CharField(max_length=200)
+    input_name = models.CharField(max_length=200)
+    file = models.FileField(null=False, blank=False)
     keywords = models.CharField(max_length=200, default="None")
     #  TODO: Will this work with the custom auth model?
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, default=None, null=True)
@@ -45,7 +26,7 @@ class DataFile(models.Model):
     source = models.CharField(max_length=200, default="test")
     institution = models.CharField(max_length=200, default="Not specified")
     version = models.PositiveIntegerField(default=1)
-    upload_date = models.DateTimeField('upload_date', default=timezone.now)
+    upload_date = models.DateTimeField(default=dateformat.format(timezone.now(), "Y-m-d H:i:s"))
     download_count = models.PositiveIntegerField(default=0)
     licence = models.CharField(max_length=200, default="[UC]2 Open Licence")
     is_invalid = models.BooleanField(null=False, default=False)
@@ -57,57 +38,27 @@ class DataFile(models.Model):
     def __file_path__(self):
         return self.file_path.value_from_object(self)
 
-    def cf_check(self):
-        return {'ok': True}
-
-    def upload(self):
-        self.upload_date = timezone.now()
-
-    def download(self):
-        self.download_count += 1
-
     class Meta:
         abstract = True
 
 
 class UC2Observation(DataFile):
-    FEATURE_TYPE = [
-        (None, "Not set (only allowed for multidimensional data)"),
-        ("timeSeries", "timeSeries"),
-        ("timeSeriesProfile", "timeSeriesProfile"),
-        ("trajectory", "trajectory"),
-    ]
-    CAMPAIGN = [
-        ("IOP01", "IOP01"),
-        ("IOP02", "IOP02"),
-        ("IOP03", "IOP03"),
-        ("IOP04", "IOP04"),
-        ("VALR01", "VALR01"),
-    ]
-    feature_type = models.CharField(max_length=32, default="Not set")
-    data_content = models.CharField(max_length=200, default="test")
+    featureType = models.CharField(max_length=32)
+    data_content = models.CharField(max_length=200)
     version = models.PositiveSmallIntegerField(default=1)
-    acronym = models.CharField(max_length=10, default="Ups")
+    acronym = models.CharField(max_length=10)
     # spatial atts
-    location = models.CharField(max_length=3, default="B")
-    site = models.CharField(max_length=12, default="Not set")
+    location = models.CharField(max_length=3)
+    site = models.CharField(max_length=12)
     #  origin_x = models.FloatField()
     #  origin_y = models.FloatField()
     #  origin_z = models.FloatField()
-    origin_lon = models.FloatField(default=0.)
-    origin_lat = models.FloatField(default=0.)
+    origin_lon = models.FloatField(default=0.0)
+    origin_lat = models.FloatField(default=0.0)
     # time atts
-    campaign = models.CharField(max_length=6, choices=CAMPAIGN, default=CAMPAIGN[0])
-    creation_time = models.CharField('creation_time', max_length=23, default=timezone.now)
-    origin_time = models.CharField(max_length=23, default=timezone.now)
-
-    def uc2_check(self):
-        ds = uc2data.Dataset(self.file_id)
-        ds.uc2_check()
-        if ds.check_result.errors:
-            print("File is not compatible with UC2 data standard.")
-            print(ds.check_result.errors)
-            return False
+    campaign = models.CharField(max_length=6)
+    creation_time = models.CharField(max_length=23)
+    origin_time = models.CharField(max_length=23)
 
 
 class Variable(models.Model):
@@ -131,4 +82,4 @@ def get_file_info(new_filename):
 
 
 def make_path():
-    return ''.join(random.choice(string.ascii_lowercase) for i in range(6))+'.nc'
+    return "".join(random.choice(string.ascii_lowercase) for i in range(6)) + ".nc"
