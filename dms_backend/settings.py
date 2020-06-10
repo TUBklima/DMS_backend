4e816fd89ddbdfd37647935696df517b93a7b709
Tom <grassmann@tu-berlin.de>
2020-06-10 13:17:35 +0200

    allow anon users to read by default

--- dms_backend/settings.py
+++ dms_backend/settings.py
@@ -109,7 +109,7 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
