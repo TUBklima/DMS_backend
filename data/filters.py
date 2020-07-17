59128c39890268eee8333c4c48f445ac34c60894
grassmann <grassmann@tu-berlin.de>
2020-07-17 09:27:52 +0200

    allow filtering by date

--- data/filters.py
+++ data/filters.py
@@ -5,6 +5,10 @@ from data.models import UC2Observation, Variable
 class UC2Filter(django_filters.FilterSet):
     acronym = django_filters.CharFilter(field_name="acronym__acronym")
     file_standard_name = django_filters.CharFilter(field_name='file_standard_name', lookup_expr='icontains')
+    upload_date = django_filters.DateFromToRangeFilter()
+    creation_time = django_filters.DateFromToRangeFilter()
+    origin_time = django_filters.DateFromToRangeFilter()
+
     class Meta:
         model = UC2Observation
         fields = {
@@ -14,7 +18,6 @@ class UC2Filter(django_filters.FilterSet):
             'author': ['exact'],
             'source': ['exact'],
             'institution': ['icontains'],
-            'upload_date': ['exact', 'year__gt', 'year__lt'],
             'is_invalid': ['exact'],
             'is_old': ['exact'],
             'version': ['exact', 'gt', 'lt'],
@@ -26,9 +29,6 @@ class UC2Filter(django_filters.FilterSet):
             'origin_lon': ['exact'],
             'origin_lat': ['exact'],
             'campaign': ['exact'],
-            # FIXME: These should refer to datetime fields
-            'creation_time': ['exact'], # 'year__gt', 'year__lt'],
-            'origin_time': ['exact'], # 'year__gt', 'year__lt'],
             'variables__id': ['exact'],
             'variables__variable': ['exact'],
             'variables__long_name': ['icontains'],
